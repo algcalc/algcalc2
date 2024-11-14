64ed5190d1dd2e60e7364db0e4146807f74b51b6
lemonsh <lemon@lemonsh.moe>
2024-11-14 14:06:06 +0100

    emu: implement input

--- emu/src/main.rs
+++ emu/src/main.rs
@@ -16,6 +16,7 @@ use os::{
 	hardware::{DisplayDriver, Hardware, Key, KeypadDriver},
 	Color,
 };
+use sdl2::{keyboard::{Keycode, Mod}, sys::KeyCode};
 
 struct Keypad(mpsc::Receiver<Key>);
 
@@ -54,6 +55,8 @@ impl<'a> DisplayDriver for Display<'a> {
 }
 
 fn main() {
+	tracing_subscriber::fmt::init();
+	
 	let (key_tx, key_rx) = mpsc::channel();
 	let keypad = Keypad(key_rx);
 
@@ -66,7 +69,7 @@ fn main() {
 		bounding_box,
 	};
 	let output_settings = OutputSettingsBuilder::new()
-		.theme(BinaryColorTheme::Inverted)
+		.theme(BinaryColorTheme::Default)
 		.max_fps(15)
 		.pixel_spacing(0)
 		.build();
@@ -80,9 +83,35 @@ fn main() {
 				match event {
 					SimulatorEvent::KeyUp {
 						keycode,
-						keymod,
-						repeat,
-					} => todo!(),
+						keymod: _,
+						repeat: _,
+					} => {
+						key_tx
+							.send(match keycode {
+								Keycode::NUM_0 | Keycode::KP_0 => Key::D0,
+								Keycode::NUM_1 | Keycode::KP_1 => Key::D1,
+								Keycode::NUM_2 | Keycode::KP_2 => Key::D2,
+								Keycode::NUM_3 | Keycode::KP_3 => Key::D3,
+								Keycode::NUM_4 | Keycode::KP_4 => Key::D4,
+								Keycode::NUM_5 | Keycode::KP_5 => Key::D5,
+								Keycode::NUM_6 | Keycode::KP_6 => Key::D6,
+								Keycode::NUM_7 | Keycode::KP_7 => Key::D7,
+								Keycode::NUM_8 | Keycode::KP_8 => Key::D8,
+								Keycode::NUM_9 | Keycode::KP_9 => Key::D9,
+								Keycode::BACKSPACE => Key::Backspace,
+								Keycode::LCTRL => Key::Fn,
+								Keycode::LEFT => Key::Left,
+								Keycode::RIGHT => Key::Right,
+								Keycode::KP_PLUS | Keycode::A => Key::Add,
+								Keycode::KP_MINUS | Keycode::S => Key::Sub,
+								Keycode::KP_MULTIPLY | Keycode::F => Key::Mul,
+								Keycode::KP_DIVIDE | Keycode::D => Key::Div,
+								Keycode::RETURN | Keycode::KP_ENTER => Key::Eq,
+								Keycode::DECIMALSEPARATOR => Key::Dot,
+								_ => continue,
+							})
+							.unwrap();
+					}
 					SimulatorEvent::Quit => process::exit(0),
 					_ => {}
 				}
